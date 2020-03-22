82de7b0fad5f0f5e848639870e1e3a8afdab83c6
Supppee <51801807+Supppee@users.noreply.github.com>
2020-03-23 00:26:22 +0100

    update db connect

--- web-application/src/data-access-layer-MySQL/db-connect.js
+++ web-application/src/data-access-layer-MySQL/db-connect.js
@@ -1,14 +1,29 @@
 const mysql = require('mysql')
-const con = mysql.createConnection({
+const config = {
     host: 'database',
     user: 'root',
     port: '3306',
     password: 'elpassword123',
     database: 'webAppDatabase'
+}
+var connection = mysql.createPool(config)
+connection.getConnection(error => {
+    if (error) console.log("Couldn't connect to database")
+    else console.log('Connected to database')
 })
-con.connect(error => {
-    if (error) console.log(error)
-    else console.log('Connected to db')
+const reconnect = connection => {
+    connection = mysql.createPool(config)
+    connection.getConnection(error => {
+        if (error) setTimeout(reconnect(connection), 5000)
+        else {
+            console.log('Connected to db')
+            return connection
+        }
+    })
+}
+
+connection.on('error', error => {
+    if (error) return reconnect(connection)
 })
 
-module.exports = con
+module.exports = connection
